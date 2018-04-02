be1c471c6d78e13de1c44036d7059b361a6628d9
DESKTOP-0NMQ8PD\Vasya <vasyamakarchuk@outlook.com>
2018-04-03 00:15:02 +0300

    not found

--- src/app/not-found/not-found.module.ts
+++ src/app/not-found/not-found.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { NotFoundComponent } from './not-found.component';
+import { NotFoundRoutingModule } from './not-found-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [NotFoundComponent]
+    imports: [
+        NotFoundRoutingModule,
+        RouterModule
+    ],
+    declarations: [NotFoundComponent]
 })
-export class NotFoundModule { }
+export class NotFoundModule {}
