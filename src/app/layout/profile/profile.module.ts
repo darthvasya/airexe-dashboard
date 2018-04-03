8056558b46b3cc5e8036e2c59fb83a6a6f41db37
DESKTOP-0NMQ8PD\Vasya <vasyamakarchuk@outlook.com>
2018-04-03 23:30:02 +0300

    finsh with profile

--- src/app/layout/profile/profile.module.ts
+++ src/app/layout/profile/profile.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { ProfileComponent } from './profile.component';
+
+import { HttpClient } from './../../shared/utils/HttpClient';
+import { AuthService } from './../../shared/core/auth.service';
+import { UserService } from './../../shared/core/user.service';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FormsModule
   ],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent],
+  providers: [UserService, HttpClient, AuthService]
 })
 export class ProfileModule { }
