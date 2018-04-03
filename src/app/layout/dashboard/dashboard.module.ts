3b201d792299c92ab33eba1eb5bff618747c241a
DESKTOP-0NMQ8PD\Vasya <vasyamakarchuk@outlook.com>
2018-04-03 19:32:00 +0300

    add widgets

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -4,14 +4,17 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
+import { Ng4TwitterTimelineModule } from 'ng4-twitter-timeline/lib/index';
+
 @NgModule({
     imports: [
         CommonModule,
         DashboardRoutingModule,
+        Ng4TwitterTimelineModule
     ],
     declarations: [
         DashboardComponent
     ],
-    providers:[]
+    providers: []
 })
 export class DashboardModule { }
