73217981ca033f1fb4a0b1a0409965f57a84176e
DESKTOP-0NMQ8PD\Vasya <vasyamakarchuk@outlook.com>
2018-04-10 14:57:21 +0300

    's'

--- src/app/layout/profile/profile.component.ts
+++ src/app/layout/profile/profile.component.ts
@@ -7,76 +7,165 @@ import { AuthService } from './../../shared/core/auth.service';
 import * as _ from 'lodash';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
-
 export class ProfileComponent implements OnInit {
-  userData: any;
-  userAttributes: Attribute[];
-  userEmail = {code: '', value: '', validation: ''};
-  userVerificationStatus = '';
-  referalId = '';
-  copyHelp  = '';
-  isCopied1 = false;
+    userData: any;
+    userAttributes: Attribute[];
+    userEmail = { code: '', value: '', validation: '' };
+    userVerificationStatus = '';
+    referalId = '';
+    copyHelp = '';
+    isCopied1 = false;
 
-  isEmailVerified = false;
-  isAccountVerified = 0;
+    isEmailVerified = false;
+    isAccountVerified = 0;
 
-  constructor(private loaderService: LoaderService, private userService: UserService, private authService: AuthService) { }
+    referrersData: any = {
+        Status0: 0,
+        Status2: 0,
+        Status6: 0,
+        Status10: 0
+    };
 
-  ngOnInit() {
-    this.loaderService.display(true);
+    staticData: any = {
+        Status0: 0,
+        Status2: 0,
+        Status6: 0,
+        Status10: 0
+    };
 
-    this.userService.getUser().then((data) => {
-      this.userData = JSON.parse(data['_body']);
-      this.fillData();
-      this.loaderService.display(false);
-    })
-    .catch(err => {
-      this.loaderService.display(false);
-    });
-  }
+    constructor(
+        private loaderService: LoaderService,
+        private userService: UserService,
+        private authService: AuthService
+    ) {}
 
-  fillData() {
-    this.userAttributes = this.userData.attrs;
-    this.userEmail = _.find(this.userData.attrs, { 'code': AttributeTypes.Email });
+    ngOnInit() {
+        this.loaderService.display(true);
 
-    this.checkBitMask(this.userData.status);
-    this.referalId = 'https://user.airexe.io/registration?referral=' + this.authService.getUserInfo().userId;
-  }
-
-  copied() {
-    this.copyHelp = 'ID copied';
-  }
-
-  checkBitMask(value) {
-    switch (value) {
-      case 0:
-        this.isEmailVerified = false;
-        this.isAccountVerified = 0;
-        break;
-      case 2:
-        this.isEmailVerified = true;
-        this.isAccountVerified = 0;
-        break;
-      case 6:
-        this.isEmailVerified = true;
-        this.isAccountVerified = 2;
-        break;
-      case 10:
-        this.isEmailVerified = true;
-        this.isAccountVerified = 1;
-        break;
+        this.getData();
+        this.getReferrers();
+        this.getStaticData();
     }
-  }
 
+    fillData() {
+        this.userAttributes = this.userData.attrs;
+        this.userEmail = _.find(this.userData.attrs, {
+            code: AttributeTypes.Email
+        });
+
+        this.checkBitMask(this.userData.status);
+        this.referalId =
+            'https://user.airexe.io/registration?referral=' +
+            this.authService.getUserInfo().userId;
+    }
+
+    copied() {
+        this.copyHelp = 'ID copied';
+    }
+
+    getData() {
+        this.userService
+            .getUser()
+            .then(data => {
+                this.userData = JSON.parse(data['_body']);
+                this.fillData();
+                this.loaderService.display(false);
+            })
+            .catch(err => {
+                this.loaderService.display(false);
+            });
+    }
+
+    getReferrers() {
+        this.userService
+            .getReferrers()
+            .then(data => {
+                const body = JSON.parse(data['_body']);
+                this.referrersData.Status0 =
+                    _.find(body, { status: 0 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 0 }).count;
+                this.referrersData.Status2 =
+                    _.find(body, { status: 2 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 2 }).count;
+                this.referrersData.Status6 =
+                    _.find(body, { status: 6 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 6 }).count;
+                this.referrersData.Status10 =
+                    _.find(body, { status: 10 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 10 }).count;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    getStaticData() {
+        this.userService
+            .getStaticData()
+            .then(data => {
+                const body = JSON.parse(data['_body']);
+
+                this.staticData.Status0 =
+                    _.find(body, { status: 0 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 0 }).airx;
+                this.staticData.Status2 =
+                    _.find(body, { status: 2 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 2 }).airx;
+                this.staticData.Status6 =
+                    _.find(body, { status: 6 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 6 }).airx;
+                this.staticData.Status10 =
+                    _.find(body, { status: 10 }) === undefined
+                        ? 0
+                        : _.find(body, { status: 10 }).airx;
+
+
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    checkBitMask(value) {
+        switch (value) {
+            case 0:
+                this.isEmailVerified = false;
+                this.isAccountVerified = 0;
+                break;
+            case 2:
+                this.isEmailVerified = true;
+                this.isAccountVerified = 0;
+                break;
+            case 6:
+                this.isEmailVerified = true;
+                this.isAccountVerified = 2;
+                break;
+            case 10:
+                this.isEmailVerified = true;
+                this.isAccountVerified = 1;
+                break;
+        }
+    }
 }
 enum AttributeTypes {
-  Email = '1050'
+    Email = '1050'
 }
 
 export class Attribute {
-  constructor(private code: string, private value: string, private validation: string) {}
+    constructor(
+        private code: string,
+        private value: string,
+        private validation: string
+    ) {}
 }
--- src/app/shared/core/user.service.ts
+++ src/app/shared/core/user.service.ts
@@ -9,44 +9,66 @@ import { saveAs } from 'file-saver/FileSaver';
 
 @Injectable()
 export class UserService {
-  constructor(private http: HttpClient, private authService: AuthService) { }
+    constructor(private http: HttpClient, private authService: AuthService) {}
 
-  getUser() {
-    return this.http.get(`${environment.apiUrl}users/` + this.authService.getUserInfo().userId);
-  }
+    getUser() {
+        return this.http.get(
+            `${environment.apiUrl}users/` +
+                this.authService.getUserInfo().userId
+        );
+    }
 
-  updateAttributes(data) {
-    return this.http.put(`${environment.apiUrl}users/${this.authService.getUserInfo().userId}/attributes`, data);
-  }
+    updateAttributes(data) {
+        return this.http.put(
+            `${environment.apiUrl}users/${
+                this.authService.getUserInfo().userId
+            }/attributes`,
+            data
+        );
+    }
 
-  createSourse(data) {
-    data.id = this.authService.getUserInfo().id;
-    data.userId = this.authService.getUserInfo().userId;
-    console.log(data);
-    return this.http.post(`${environment.apiUrl}users/${this.authService.getUserInfo().userId}/sources`, data);
-  }
+    createSourse(data) {
+        data.id = this.authService.getUserInfo().id;
+        data.userId = this.authService.getUserInfo().userId;
+        return this.http.post(
+            `${environment.apiUrl}users/${
+                this.authService.getUserInfo().userId
+            }/sources`,
+            data
+        );
+    }
 
-  createSourceByForm(data, picture: File) {
-    console.log(picture);
+    createSourceByForm(data, picture: File) {
 
-    let formData: FormData = new FormData();
-    formData.append('Picture', picture, 'file.jpg');
+        const formData: FormData = new FormData();
+        formData.append('Picture', picture, 'file.jpg');
 
-    console.log(formData);
-    // tslint:disable-next-line:max-line-length
-    return this.http.post(`${environment.apiUrl}users/${this.authService.getUserInfo().userId}/sources/${data.sourceType}/form`, formData);
-  }
+        // tslint:disable-next-line:max-line-length
+        return this.http.post(
+            `${environment.apiUrl}users/${
+                this.authService.getUserInfo().userId
+            }/sources/${data.sourceType}/form`,
+            formData
+        );
+    }
 
-  getSourse(id) {
+    getSourse(id) {
+        return this.http
+            .download(`${environment.apiUrl}sources/${id}`)
+            .then(data => {
+                const blob = new Blob([data['_body']], { type: 'image/jpg' });
+                saveAs(blob, `${id}.jpg`);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-    return this.http.download(`${environment.apiUrl}sources/${id}`).then((data) => {
-            console.log(data);
-            const blob = new Blob([data['_body']] , {type: 'image/jpg'});
-            saveAs(blob, `${id}.jpg`);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    getReferrers() {
+        return this.http.get(`${environment.apiUrl}users/` + this.authService.getUserInfo().userId + `/referrers`);
+    }
 
-  }
+    getStaticData() {
+        return this.http.get(`../../../assets/data/referrers.json`);
+    }
 }
